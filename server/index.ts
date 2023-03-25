c3232ba76ab26768eb822b2cbed3d4d92b575b3c
WintrCat <wintrcat.wilson@gmail.com>
2023-03-25 01:23:42 +0000

    Initial

---
+++ server/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import path from "path";
+
+let server = express();
+
+// middleware
+server.use(express.json());
+server.use("/client", express.static("../client"));
+server.use("/assets", express.static("../assets"));
+
+// routes
+server.get("/", (req, res): void => {
+    res.sendFile(path.resolve("../client/login/index.html"));
+});
+
+server.get("/register", (req, res): void => {
+
+});
+
+// listen pls bruh
+server.listen(10003, () => {
+    console.log("Server running on port 10003.");
+});
